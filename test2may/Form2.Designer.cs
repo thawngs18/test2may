88d534c4cd92bf702891bb65135fe8e3b109130b
Thắng Phan Đức <apgpdt10@gmail.com>
2024-11-26 00:08:55 +0700

    123

--- test2may/Form2.Designer.cs
+++ test2may/Form2.Designer.cs
@@ -35,7 +35,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(566, 49);
+			this.label1.Location = new System.Drawing.Point(317, 76);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(439, 25);
 			this.label1.TabIndex = 0;
